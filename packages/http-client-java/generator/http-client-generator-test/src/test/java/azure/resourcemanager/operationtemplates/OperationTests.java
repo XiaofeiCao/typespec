e4494bc6862b2f3df6e010fa04fc9bd09d77f489
XiaofeiCao <xiaofeicao@microsoft.com>
2025-06-20 17:12:16 +0800

    fix test

--- packages/http-client-java/generator/http-client-generator-test/src/test/java/azure/resourcemanager/operationtemplates/OperationTests.java
+++ packages/http-client-java/generator/http-client-generator-test/src/test/java/azure/resourcemanager/operationtemplates/OperationTests.java
@@ -32,21 +32,26 @@ public class OperationTests {
         Assertions.assertEquals("A test widget", widget.properties().description());
         widget = manager.optionalBodies().patch(rgName, resourceName);
         Assertions.assertEquals("A test widget", widget.properties().description());
+
+
+        ChangeAllowanceResult result = manager.optionalBodies().providerPost();
+        Assertions.assertEquals(50, result.totalAllowed());
+        Assertions.assertEquals("Changed to default allowance", result.status());
+        result = manager.optionalBodies().providerPostWithResponse(new ChangeAllowanceRequest().withReason("Increased demand").withTotalAllowed(100), Context.NONE).getValue();
+        Assertions.assertEquals(100, result.totalAllowed());
+        Assertions.assertEquals("Changed to requested allowance", result.status());
+
         WidgetInner inner = widget.innerModel();
         inner.withProperties(new WidgetProperties().withName("updated-widget").withDescription("Updated description"));
         widget = manager.optionalBodies().patchWithResponse(rgName, resourceName, inner, Context.NONE).getValue();
-        Assertions.assertEquals("updated-widget", widget.properties().description());
+        Assertions.assertEquals("Updated description", widget.properties().description());
+        Assertions.assertEquals("updated-widget", widget.properties().name());
 
         ActionResult actionResult = manager.optionalBodies().post(rgName, resourceName);
         Assertions.assertEquals("Action completed successfully", actionResult.result());
 
         actionResult = manager.optionalBodies().postWithResponse(rgName, resourceName, new ActionRequest().withActionType("perform").withParameters("test-parameters"), Context.NONE).getValue();
         Assertions.assertEquals("Action completed successfully with parameters", actionResult.result());
-
-        ChangeAllowanceResult result = manager.optionalBodies().providerPost();
-        Assertions.assertEquals(50, result.totalAllowed());
-        result = manager.optionalBodies().providerPostWithResponse(new ChangeAllowanceRequest().withReason("Increased demand").withTotalAllowed(100), Context.NONE).getValue();
-        Assertions.assertEquals(100, result.totalAllowed());
     }
 
     // for LRO operations, we need to override default poll interval
